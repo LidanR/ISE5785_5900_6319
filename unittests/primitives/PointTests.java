f8dd93fc4961bca20b5947a15dbe0bdfa62c7636
Dror_Yakov <dror200504@gmail.com>
2025-03-25 18:12:17 +0200

    added tests for vector and bva tests for point

--- unittests/primitives/PointTests.java
+++ unittests/primitives/PointTests.java
@@ -8,26 +8,28 @@ class PointTests {
     Point p1 = new Point(1, 2, 3);
     Point p2 = new Point(2, 3, 4);
     Vector v1 = new Vector(1, 1, 1);
+    Vector v2 = new Vector(-1, -2, -3);
 
     @Test
     void testAdd() {
         // ============ Equivalence Partitions Tests ==================
         // TC01: Simple test for add
-        Point p2 = p1.add(v1);
-        assertEquals(new Point(2, 3, 4), p2, "Simple test for add");
+        assertEquals(new Point(2, 3, 4), p1.add(v1), "Simple test for add");
 
+        // ============ Boundary Value Tests ==================
+        // TC02: Adding the opposite vector to a point should return ZeroPoint
+        assertEquals(Point.ZERO, p1.add(v2), "Simple test for add");
     }
 
     @Test
     void testSubtract() {
         // ============ Equivalence Partitions Tests ==================
         // TC01: Simple test for subtract
-        Vector v1 = p2.subtract(p1);
-        assertEquals(new Vector(1, 1, 1), v1, "Simple test for subtract");
+        assertEquals(new Vector(1, 1, 1), p2.subtract(p1), "Simple test for subtract");
 
+        // ============ Boundary Value Tests ==================
         // TC02: Subtracting a point from itself should return the zero vector
-        Vector v2 = p1.subtract(p1);
-        assertEquals(new Vector(0, 0, 0), v2, "Subtracting a point from itself should return the zero vector");
+        assertThrows(IllegalArgumentException.class, () -> p1.subtract(p1), "Boundary test for subtract");
     }
 
     @Test
@@ -37,17 +39,15 @@ class PointTests {
         double d = p1.distanceSquared(p2);
         assertEquals(3, d, "Simple test for distanceSquared");
 
+        // ============ Boundary Value Tests ==================
         // TC02: BVA test for distanceSquared
-        Point p3 = new Point(1, 2, 3);
-        double d2 = p3.distanceSquared(p1);
-        assertEquals(0, d2, "Squared distance between the same point should be 0");
+        assertEquals(0, p1.distanceSquared(p1), "Squared distance between the same point should be 0");
     }
 
     @Test
     void testDistance() {
         // ============ Equivalence Partitions Tests ==================
         // TC01: Simple test for distance
-        double d = p1.distance(p2);
-        assertEquals(Math.sqrt(3), d, "Simple test for distance");
+        assertEquals(Math.sqrt(3), p1.distance(p2), "Simple test for distance");
     }
 }
--- unittests/primitives/VectorTests.java
+++ unittests/primitives/VectorTests.java
@@ -5,20 +5,34 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class VectorTests {
-    Point p1 = new Point(1, 2, 3);
-    Vector v1 = new Vector(1, 1, 1);
-    Vector v2 = new Vector(0,0,0);
+    Vector v1 = new Vector(1, 2, 3);
+    Vector v2 = new Vector(2,3,4);
+
 
     @Test
     void testSubtract() {
+        // ============ Equivalence Partitions Tests ==================
+        // TC01: Simple test for subtract
+        assertEquals(new Vector(1, 1, 1), v2.subtract(v1), "Simple test for subtract");
+
+        // ============ Boundary Value Tests ==================
+        //TC02: Boundary test for subtract
+        assertThrows(IllegalArgumentException.class, () -> v1.subtract(v1), "Boundary test for subtract");
         }
 
     @Test
     void testAdd() {
         // ============ Equivalence Partitions Tests ==================
         // TC01: Simple test for add
-        Point p2 = p1.add(v1);
-        assertEquals(new Point(2, 3, 4), p2, "Simple test for add");
+        assertEquals(new Point(3, 4, 5), v1.add(v2), "Simple test for add");
+
+        // ============ Boundary Value Tests ==================
+        //TC02: Boundary test for add
+        assertEquals(new Vector(2, 3, 6), v1.add(v1), "Boundary test for add");
+
+        //TC03: Boundary test for add
+        Vector v4 = new Vector(-1, -2, -3);
+        assertThrows(IllegalArgumentException.class, () -> v1.add(v4), "Boundary test for add");
        }
 
     @Test
@@ -26,10 +40,6 @@ class VectorTests {
         // ============ Equivalence Partitions Tests ==================
         // TC01: Simple test for lengthSquared
         assertEquals(3, v1.lengthSquared(), "Simple test for lengthSquared");
-
-        // ============ Equivalence Partitions Tests ==================
-        // TC02: BVA test for length
-        assertEquals(0, v2.length(), "BVA test for length");
     }
 
     @Test
@@ -37,46 +47,32 @@ class VectorTests {
         // ============ Equivalence Partitions Tests ==================
         // TC01: Simple test for length
         assertEquals(Math.sqrt(3), v1.length(), "Simple test for length");
-
-        // ============ Equivalence Partitions Tests ==================
-        // TC02: BVA test for length
-        assertEquals(0, v2.length(), "BVA test for length");
     }
 
     @Test
     void testScale() {
         // ============ Equivalence Partitions Tests ==================
         // TC01: Simple test for scale
-        assertEquals(new Vector(2, 2, 2), v1.scale(2), "Simple test for scale");
+        assertEquals(new Vector(2, 4, 6), v1.scale(2), "Simple test for scale");
 
-        // ============ Equivalence Partitions Tests ==================
+        // ============ Boundary Value Tests ==================
         // TC02: BVA test for scale
-        assertEquals(new Vector(0,0,0), v1.scale(0), "BVA test for scale");
-
-        // ============ Equivalence Partitions Tests ==================
-        // TC03: BVA test for scale
-        assertEquals(new Vector(0,0,0), v2.scale(2), "BVA test for scale");
+        assertThrows(IllegalArgumentException.class, () -> v1.scale(0), "BVA test for scale");
     }
 
     @Test
     void testDotProduct() {
-        Vector v1 = new Vector(1, 2, 3);
-        Vector v2 = new Vector(2, 3, 4);
-        // ============ Equivalence Partitions Tests ==================
-        // TC02: Simple test for dotProduct
-        assertEquals(20, v1.dotProduct(v2), "Simple test for dotProduct");
-
         Vector v3 = new Vector(1, 0, 0);
         Vector v4 = new Vector(1, 1, 0);
         // ============ Equivalence Partitions Tests ==================
         // TC03: Simple test for dotProduct
-        assertEquals(1, v3.dotProduct(v4), "Simple test for dotProduct");
+        assertEquals(1, v3.dotProduct(v4), "0-90 degree test for dotProduct");
 
         Vector v5 = new Vector(1, 0, 0);
         Vector v6 = new Vector(-1, 1, 0);
         // ============ Equivalence Partitions Tests ==================
         // TC04: Simple test for dotProduct
-        assertEquals(-1, v5.dotProduct(v6), "Simple test for dotProduct");
+        assertEquals(-1, v5.dotProduct(v6), "90-180 degree test for dotProduct");
 
         Vector v7 = new Vector(1, 0, 0);
         Vector v8 = new Vector(0, 1, 0);
@@ -93,10 +89,20 @@ class VectorTests {
         Vector v2 = new Vector(2, 3, 4);
         assertEquals(new Vector(-1, 2, -1), v1.crossProduct(v2), "Simple test for crossProduct");
 
-
+        // ============ Equivalence Partitions Tests ==================
+        //TC02: BVA test for crossProduct
+        Vector v3 = new Vector(2, 4, 6);
+        assertEquals(new Vector(0,0,0), v1.crossProduct(v3), "BVA test for crossProduct");
     }
 
     @Test
     void testNormalize() {
+        // ============ Equivalence Partitions Tests ==================
+        // TC01: Simple test for normalize// TC01: Simple test for normalize
+        assertEquals(1, v1.normalize().length(), "Simple test for normalize");
+
+        // TC02: Check that the normalized vector has the same direction
+        Vector normalizedV1 = v1.normalize();
+        assertTrue(normalizedV1.dotProduct(v1) > 0, "Normalized vector should have the same direction");
     }
 }
